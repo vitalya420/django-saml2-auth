75ec9d1aa7ecd5b1bc416d90d8df3af3cbb286b1
Vitaliy <67874485+vitalya420@users.noreply.github.com>
2022-12-19 15:31:03 +0200

    re_path

--- django_saml2_auth/urls.py
+++ django_saml2_auth/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
 app_name = 'django_saml2_auth'
 
 urlpatterns = [
-    url(r'^acs/$', views.acs, name="acs"),
-    url(r'^welcome/$', views.welcome, name="welcome"),
-    url(r'^denied/$', views.denied, name="denied"),
+    re_path(r'^acs/$', views.acs, name="acs"),
+    re_path(r'^welcome/$', views.welcome, name="welcome"),
+    re_path(r'^denied/$', views.denied, name="denied"),
 ]
